84f6e8247e785f4b39bda795c9ed46ac3d04f4e2
Robinkalkan <Robin.kalkan@chasacademy.se>
2024-11-28 20:28:01 +0100

    Add project files.

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using Labb2RestaurantMVC.Models.Menu;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Labb2RestaurantMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly HttpClient _client;
+        private string baseUri = "https://localhost:7073/";
+        public MenuController(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Menu";
+            ViewBag.PageTitle = "Menu List";
+
+            var response = await _client.GetAsync($"{baseUri}api/Menu/GetAllAvailableFoodMenu");
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var menuList = JsonConvert.DeserializeObject<List<Menu>>(json);
+
+            return View(menuList);
+        }
+    }
+}
